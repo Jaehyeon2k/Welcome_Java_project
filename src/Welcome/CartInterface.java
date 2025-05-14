1e93ff6154dbd6cb34d174ca122736bbb4fc4c92
김재현 <wmspfl2673@gmail.com>
2025-05-14 15:42:31 +0900

    9

---
+++ src/Welcome/CartInterface.java
@@ -0,0 +1,9 @@
+package Welcome;
+
+public interface CartInterface {
+	void printBookList(Book[] p);
+	boolean isCartInBook(String id);
+	void insertBook(Book p);
+	void removeCart(int numId);
+	void deleteBook();
+}
